08109f30a46a208979408d604ebcd4a6714be115
Ansh Mahajan <anshmahajan678@gmail.com>
2026-01-16 18:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let count = 0;
+
+let counter = document.getElementById("counter");
+let btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click", function () {
+  alert(count);          // show un-incremented value
+  count++;               // increment
+  counter.innerHTML = count; // update UI
+});
